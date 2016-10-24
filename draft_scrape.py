ffb2e115bead5d13d983e5c3059a12369ebc71ba
Jules Sulpico <Kamdori@pcp138752pcs.wireless.calpoly.edu>
2016-10-24 16:38:13 -0700

    Created scraper for training draft text files. kevin was here.

--- draft_scrape.py
+++ draft_scrape.py
@@ -1,16 +1,30 @@
+import json
+from os import listdir
+from os.path import isfile, join
 
+path_of_drafts = './training_drafts'
 
-def main():
-    lst = [1,2,3,]
+def scrape_drafts(files):
+    drafted_decks = []
     # open the text file
-    for item in lst:
-        with open(item, 'r') as file_in:
+    print len(files)
+    for item in files:
+        with open(path_of_drafts + '/' + item, 'r') as file_in:
+            deck = []
             for line in file_in.readlines():
                 if line[:4] == '--> ':
                     card = line[4:]
-                    print (card)
-    # scrape the card choices
+                    deck.append(card)
+        drafted_decks.append(deck)
+    return drafted_decks
 
+def to_json_dump(to_dump):
+    json.dumps(to_dump)
+
+def main():
+    files = [f for f in listdir(path_of_drafts) if isfile(join(path_of_drafts, f))]
+    to_dump = scrape_drafts(files)
+    to_json_dump(to_dump)
 
 if __name__ == '__main__':
   main()
