0ee751c2f10f5075e1c735cc03bd3e85ec97b59f
Kevin Sogata <ksogata@gmail.com>
2016-11-15 16:21:20 -0800

    added py file that creates a text file of all drafts of 45 in scraped_drafts. Each draft is on one line and seperated by '\n'. Kevin wuz here.

---
+++ src/app/draft_scraper/format_draft.py
@@ -0,0 +1,44 @@
+from os import listdir
+from os.path import isfile, join
+
+## Enter directory of drafts dump
+path_of_drafts = '../../data/kaladesh/scraped_drafts'
+
+## Open each draft file and scrape the lines beginning with '--> '
+def scrape_drafts(files):
+    drafted_decks = []
+    # open the text file
+    # print len(files)
+    for item in files:
+        with open(path_of_drafts + '/' + item, 'r') as file_in:
+            deck = []
+            for line in file_in.readlines():
+                if line[:4] == '--> ':
+                    card = line[4:]
+                    deck.append(card)
+        drafted_decks.append(deck)
+    return drafted_decks
+
+## Write each drafted deck on a line in a text file.
+def write_to_file(drafts):
+    for draft in drafts:
+        with open('all_drafts.txt', 'a') as file_in:
+            for card in draft:
+                if (card[:3] != 'Bot'):
+                    f_card = card.replace(' ', '_')
+                    f_card = f_card.replace('\n', '')
+                    # print(f_card)
+                    file_in.write(f_card + ' ')
+            file_in.write('\n')
+
+## Gets list of files in "path_of_drafts"
+## Scrape drafts for cards picked
+## Send to NN...? Come back to this.
+def main():
+    files = [f for f in listdir(path_of_drafts) if isfile(join(path_of_drafts, f))]
+    to_write = scrape_drafts(files)
+    write_to_file(to_write)
+    
+
+if __name__ == '__main__':
+    main()
